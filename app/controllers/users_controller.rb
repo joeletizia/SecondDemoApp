6557a709b3c71c70f424333857f9d9e89474bedb
Joe Letizia <joe.letizia@gmail.com>
2012-09-26 20:44:13 -0400

    finish user edit, update, destroy, and index features.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit,:update]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user, only: [:destroy]
 
   def new
     @user = User.new
@@ -27,6 +28,12 @@ class UsersController < ApplicationController
   
   def edit
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_url
+  end
 
   def update
     if @user.update_attributes(params[:user])
@@ -52,6 +59,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
   
 end
